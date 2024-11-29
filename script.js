c47deb416ee08cd1a26393a510b0826b41cd3482
Ruth Laird <ruth.laird@anaplan.com>
2024-11-29 08:52:50 +0000

    starter project

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Selecting elements
+const score0Element = document.querySelector("#score--0");
+const score1Element = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+// Starting conditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add("hidden");
